243690809f263023392a4c55e9d5d5bf3b13192a
rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-10 23:40:39 +0000

    May 10, 2023, 4:40 PM

--- events/api_views.py
+++ events/api_views.py
@@ -54,7 +54,8 @@ class ConferenceListEncoder(ModelEncoder):
     model = Conference
     properties = ["name"]
 
-def api_list_conferences(request):
+@require_http_methods(request_method_list=["GET", "POST"])
+def api_list_conferences(request):    
     """
     Lists the conference names and the link to the conference.
 
@@ -73,11 +74,32 @@ def api_list_conferences(request):
         ]
     }
     """
-    conferences = Conference.objects.all()
-    return JsonResponse(
-        {"conferences": conferences},
-        encoder=ConferenceListEncoder,
-    )
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences},
+            encoder=ConferenceListEncoder,
+            safe=False
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+
+        # Get the Location object and put it in the content dict
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference,
+            encoder=ConferenceDetailEncoder,
+            safe=False,
+        )
+
 
 
 @require_http_methods(request_method_list=["GET", "PUT", "DELETE"])
