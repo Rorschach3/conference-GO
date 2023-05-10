e4b12286f5c189ecfa1b8f9e044098e19bd81170
rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-10 21:56:43 +0000

    May 10, 2023, 2:56 PM

--- events/api_views.py
+++ events/api_views.py
@@ -143,17 +143,16 @@ def api_list_locations(request):
         )
     else:
         content = json.loads(request.body)
-
         # Get the State object and put it in the content dict
-try:
-    state = State.objects.get(abbreviation=content["state"])
-    content["state"] = state
-except State.DoesNotExist:
-    return JsonResponse(
-        {"message": "Invalid state abbreviation"},
-        status=400,
-    )
-    
+        try:
+            state = State.objects.get(abbreviation=content["state"])
+            content["state"] = state
+        except State.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid state abbreviation"},
+                status=400,
+            )
+            
     """
     Returns the details for the Location model specified
     by the id parameter.
