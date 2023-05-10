5344738b94f6d2085e508c99cdc9f23e3bd3b684
rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-10 00:30:27 +0000

    May 9, 2023, 5:30 PM

--- common/json.py
+++ common/json.py
@@ -4,16 +4,13 @@ from json import JSONEncoder
 class ModelEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, self.model):
-        #   if the object to decode is the same class as what's in the
-        #   model property, then
-        #     * create an empty dictionary that will hold the property names
-        #       as keys and the property values as values
+            d = {}
         #     * for each name in the properties list
         #         * get the value of that property from the model instance
         #           given just the property name
         #         * put it into the dictionary with that property name as
         #           the key
-        #     * return the dictionary
+            return d
         else:
             return super().default(0)
         pass
