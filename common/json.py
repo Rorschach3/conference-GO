afba8b2f8a7bc1cb677c7b0ed3cb7f77dc04c6ef
rorschach3 <daniel_fhernandez@yahoo.com>
2023-05-10 00:32:29 +0000

    May 9, 2023, 5:32 PM

--- common/json.py
+++ common/json.py
@@ -5,11 +5,8 @@ class ModelEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, self.model):
             d = {}
-        #     * for each name in the properties list
-        #         * get the value of that property from the model instance
-        #           given just the property name
-        #         * put it into the dictionary with that property name as
-        #           the key
+            for property in self.properties:
+                d.append(
             return d
         else:
             return super().default(0)
